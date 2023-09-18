f2fe7b6f59191e793bf99493a7de503936d0d772
JuanJoseVillamizarCampus1 <juanjvs1409@gmail.com>
2023-09-18 07:39:37 -0500

    refactor: :rocket: endpoint 7

--- controllers/compras.js
+++ controllers/compras.js
@@ -2,13 +2,25 @@
 const {getCollection}= require('../controllers/medicamentos')
 const {response} = require('express')
 async function ventasProve(req,res=response) {
-    const collection = await getCollection('Compras')
-    const documento = await collection.aggregate([
-        {$:{
-            "proveedor.nombre" :"ProveedorB"
-        }}
-    ]).toArray()
-    res.json(documento)
+    try {
+        const collection = await getCollection('Compras');
+        const documento = await collection.aggregate([
+          {
+            $unwind: "$medicamentosComprados"
+          },
+          {
+            $group: {
+              _id: "$proveedor.nombre",
+              totalMedicamentosVendidos: {
+                $sum: "$medicamentosComprados.cantidadComprada"}
+            }
+          }
+        ]).toArray();
+        res.json(documento)
+    } catch (error) {
+        console.log(error);
+    }
+   
 }
 module.exports= {
     ventasProve
--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@
 4.Obtener recetas médicas emitidas después del 1 de enero de 2023. --> localhost:8001/api/ventas/recetas
 5.Total de ventas del medicamento ‘Paracetamol’. --> localhost:8001/api/ventas/paracetamol
 6.Medicamentos que caducan antes del 1 de enero de 2024. --> localhost:8001/api/medicamentos/caducidad
-7.Total de medicamentos vendidos por cada proveedor.
+7.Total de medicamentos vendidos por cada proveedor. --> localhost:8001/api/compras/proveedor
